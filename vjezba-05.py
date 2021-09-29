7778152fbef11edf3bc2d65081cdc8252aafb7ed
LaraDuretic <91601295+LaraDuretic@users.noreply.github.com>
2021-09-29 12:03:36 +0200

    Add files via upload

---
+++ vjezba-05.py
@@ -0,0 +1,13 @@
+a = int(input("Unesi broj a:"))
+
+b = int(input("Unesi broj b:"))
+
+suma = a + b
+
+razlika= a - b
+
+količnik= a / b
+
+umnožak= a * b
+
+print("suma: {0} razlika: {1} količnik: {2} umnožak: {3}".format(suma, razlika, količnik, umnožak))
